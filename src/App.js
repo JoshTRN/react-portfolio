d16d2b28312a52e951fddb2af8b1dd5efd8a36fa
Josh <jrodwod@gmail.com>
2019-04-22 22:24:32 -0400

    Added PFP and Electron Started Nav

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Nav from './Components/Nav'
+import TechBar from './Components/TechBar'
+import "./App.css"
 
-class App extends Component {
-  render() {
+function App() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div id="wrapper">
+            <Nav />
+            <img id="profile-pic" src="./img/profile-pic.png" alt="Josh-PFP"/>
+            <TechBar />
+        </div>
     );
-  }
 }
-
 export default App;
---
+++ src/Components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import Typed from "typed";
+
+export default function Nav() {
+
+    // const typed = new Typed("element", {});
+
+    // console.log(typed);
+
+    return <nav>Nav</nav>;
+}
